16b0fc9176a527d457f0c32d0057060eddc5dd2e
j.abduxakimov <j.abduxakimov@newuu.uz>
2025-08-27 14:47:56 +0500

    solved all problems

---
+++ Lab1_problem3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab1_problem3
+//
+//  Created by Jahongir Abduxakimov on 27/08/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
