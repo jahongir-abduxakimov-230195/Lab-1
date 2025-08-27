8d127b57f930e57053a8cdf7465cb9fc5db01bfe
j.abduxakimov <j.abduxakimov@newuu.uz>
2025-08-27 13:33:29 +0500

    Initial Commit

---
+++ Lab 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 1
+//
+//  Created by Jahongir Abduxakimov on 27/08/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
